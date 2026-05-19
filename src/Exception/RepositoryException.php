98608e22e72292bbb56521decd536b58faf8cde0
elbor870 <elikbor63395@gmail.com>
2026-05-19 11:53:42 +0700

    Update RepositoryException.php

--- src/Exception/RepositoryException.php
+++ src/Exception/RepositoryException.php
@@ -1,1 +1,10 @@
+<?php
+declare(strict_types=1);
 
+class RepositoryException extends \Exception
+{
+    public function __construct(string $message, int $code = 0, ?\Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
